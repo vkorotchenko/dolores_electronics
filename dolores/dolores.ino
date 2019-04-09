12d03087ae950f09ba251f1545ee1951da24fb64
Korotchenko, Vadim <vkorotchenko@paypal.com>
2019-04-08 17:53:21 -0700

    adding functionality

--- dolores/dolores.ino
+++ dolores/dolores.ino
@@ -37,12 +37,14 @@ const uint8_t SEG_OIL[] = {
 #define TURN_SIGNAL_BLINK_DELAY 500
 #define VOLTAGE_THRESHOLD 12.8
 #define RUNNING_DISTANCE_THRESHOLD 1.00f
-#define EE_ADDRESS 0
+#define EE_ODOMETER_ADDRESS 0
+#define EE_CHECK_ADDRESS 99
+#define EE_METRIC_ADDRESS 77
+#define TIME_SINCE_STARTED_THRESHOLD 1000
+#define SCROLL_SPEED 400
 
 
 // VOLTAGE SENSOR VALUES
-float vOUT = 0.0;
-float vIN = 0.0;
 float R1 = 30000.0;
 float R2 = 7500.0;
 
@@ -55,6 +57,8 @@ bool isLeftTurnOn = false;
 bool isRightTurnOn = false;
 bool isHeadlightOn = false;
 
+long millisWhenStarted = 0;
+
 //decleare Bounce
 Bounce debouncerRightTurn = Bounce();
 Bounce debouncerLeftTurn = Bounce();
@@ -65,7 +69,7 @@ NeoSWSerial gpsSerial(GPS_TX, GPS_RX);
 NMEAGPS gps;
 
 NeoGPS::Location_t prev_location;
-bool firstScan = true;
+bool firstLocationScan = true;
 float runningDistance = 0;
 
 
@@ -76,14 +80,16 @@ void setup()
 {
   // Display settings
   display.setBrightness(0x09);
-  setKph();
+
+  //INIT EEPROM
+  initEEPROM();
 
   //init serial
   Serial.begin(115200);
 
   // initialize GPS
   gpsSerial.begin(9600);
-  gps.send_P( &gpsSerial, F("PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0") ); // RMC(Recomended minim specific) only
+  gps.send_P( &gpsSerial, F("PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")); // RMC(Recomended minim specific) only
   gps.send_P( &gpsSerial, F("PMTK220,1000") ); // 4Hz update
 
   // set pin modes
@@ -108,8 +114,18 @@ void setup()
   pinMode(RIGHT_TURN_RELAY, OUTPUT);
   pinMode(LEFT_TURN_RELAY, OUTPUT);
   pinMode(HEAD_LIFGHT_RELAY, OUTPUT);
-  pinMode(AUX_RELAY, OUTPUT);
+  pinMode(STARTER_RELAY, OUTPUT);
+  pinMode(HORN_RELAY, OUTPUT);
 
+  //ELECTRONICS TEST
+  digitalWrite(RIGHT_TURN_RELAY, HIGH);
+  digitalWrite(LEFT_TURN_RELAY, HIGH);
+
+  //DISPLAY ODOMETER
+  scrollOdometer();
+
+  digitalWrite(RIGHT_TURN_RELAY, LOW);
+  digitalWrite(LEFT_TURN_RELAY, LOW);
 }
 
 void loop()
@@ -141,21 +157,23 @@ void loop()
     }
 
     if (fix.valid.location) {
-      if (firstScan) {
-         firstScan = false;
+      if (firstLocationScan) {
+         firstLocationScan = false;
          prev_location = fix.location;
        } else {
          float range = fix.location.DistanceKm (prev_location);
          runningDistance = runningDistance + range;
          if (runningDistance > RUNNING_DISTANCE_THRESHOLD) {
            persistRange(range);
+           runningDistance = 0;
+         }
        }
-
+    }
   }
 
-  debouncerRightTurn.update ( );
-  debouncerLeftTurn.update ( );
-  debouncerHeadlight.update ( );
+  debouncerRightTurn.update();
+  debouncerLeftTurn.update();
+  debouncerHeadlight.update();
 
   //read left turn
   if (debouncerLeftTurn.read() == HIGH) {
@@ -184,8 +202,8 @@ void loop()
   if (isMotorcycleRunning()) {
     //CHECK OIL
     if( digitalRead(OIL_SENSOR) == HIGH) {
-      display.setSegments(SEG_OIL)
-      continue;
+      display.setSegments(SEG_OIL);
+      //continue;
     }
     digitalWrite(HORN_RELAY, digitalRead(AUX_IN));
   } else {
@@ -195,19 +213,57 @@ void loop()
 }
 
 void persistRange(float range) {
-  //TODO check if value is valid (case for first time)
   float odometer = 0.00f;
-  EEPROM.get( EE_ADDRESS, odometer);
+  EEPROM.get( EE_ODOMETER_ADDRESS, odometer);
   odometer = odometer + range;
-  EEPROM.update( EE_ADDRESS, odometer);
+  EEPROM.update( EE_ODOMETER_ADDRESS, odometer);
+}
+
+void scrollOdometer() {
+    float value;
+    EEPROM.get(EE_ODOMETER_ADDRESS, value);
+    int odometer = (int) value;
+
+    int digits = ((int) pow(odometer , 0.1)) + 1;
+
+    for (int i = 0; i < digits ; i++) {
+      int display_value = getDisplayValue(digits, i, odometer);
+      display.showNumberDec(display_value, i > 3, 4, 0);
+      delay(SCROLL_SPEED);
+    }
+      delay(SCROLL_SPEED);
+}
+
+int getDisplayValue(int digits, int offset ,int reading) {
+    int x0 = extractDigit(reading, digits-offset);
+    int x1 = extractDigit(reading, digits-offset + 1);
+    int x2 = extractDigit(reading, digits-offset + 2);
+    int x3 = extractDigit(reading, digits-offset + 3);
+
+    return (x3 * 1000) + (x2 * 100) + (x1 * 10) + x0;
+}
+
+int extractDigit(int v, int p){
+  return int(v/(pow(10,p-1))) - int(v/(pow(10,p)))*10;
 }
 
 boolean isMotorcycleRunning() {
-  value = analogRead(VOLT_SENSOR);
-  vOUT = (value * 5.0) / 1024.0;
-  vIN = vOUT / (R2/(R1+R2));
-  return vIn > VOLTAGE_THRESHOLD;
-  // TODO keep track for a couple of seconds before switching to true;
+  int value = analogRead(VOLT_SENSOR);
+  float vOUT = (value * 5.0) / 1024.0;
+  float vIN = vOUT / (R2/(R1+R2));
+  if( vIN > VOLTAGE_THRESHOLD) {
+    if(millisWhenStarted = 0) {
+       millisWhenStarted = millis();
+       return false;
+    }
+    else if(millisWhenStarted < millis() - TIME_SINCE_STARTED_THRESHOLD) {
+        return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 }
 
 void blinkTurnSignal(bool isOn, int pin) {
@@ -228,10 +284,26 @@ void setKph() {
   uint8_t kph[] = { 0x00, 0x00, 0x00, SEG_A };
   display.setSegments(kph);
   isMetric = true;
+  EEPROM.update(EE_METRIC_ADDRESS, isMetric);
 }
 
 void setMph() {
   uint8_t kph[] = { 0x00, 0x00, 0x00, SEG_D };
   display.setSegments(kph);
   isMetric = false;
+  EEPROM.update(EE_METRIC_ADDRESS, isMetric);
+}
+
+void initEEPROM() {
+  int check_value;
+  EEPROM.get(EE_CHECK_ADDRESS, check_value);
+
+  if( check_value == 255){
+    EEPROM.put(EE_ODOMETER_ADDRESS, 0);
+    EEPROM.put(EE_METRIC_ADDRESS, isMetric);
+    setKph();
+    EEPROM.put(EE_CHECK_ADDRESS, 0);
+  } else {
+    EEPROM.get(EE_METRIC_ADDRESS, isMetric);
+  }
 }
